dca8c88d69ac353de377f8504eb2a3e28629b67d
unknown <pedronym18@gmail.com>
2020-11-11 02:14:19 -0500

    Tercera prueba

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    alert("funciono");
+$(function(){
+    $(`ul.tabs li a:first`).addClass(`active2`);
+    $(`.secciones article`).hide();
+    $(`.secciones article:first`).show();
+
+    $(`ul.tabs li a`).click(function(){
+        $(`ul.tabs li a`).removeClass('active2');
+        $(this).addClass('active2');
+        $(`.secciones article`).hide();
+
+        var activeTab=$(this).attr(`href`);
+        console.log(activeTab);
+        $(activeTab).show();
+        return false;
+    });
 });
